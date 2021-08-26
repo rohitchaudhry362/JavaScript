0c6fcaf3d8474e436f88769b07cafa18c364564a
Rohit Chaudhry <rohitchaudhry362@gmail.com>
2021-08-26 10:53:21 -0400

    Added JavaScript Array Every method

---
+++ JavaScript Every Method.js
@@ -0,0 +1,8 @@
+// JavaScript eveery method checks if every elemetn in the array
+// passes the test or not
+
+const numbers = [1,2,-9,7]
+
+const positives = numbers.every(x => x > 0);
+
+console.log(positives);
