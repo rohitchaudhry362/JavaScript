2e573798f45cfbc4cd445834f4c585b1d301bb19
Rohit Chaudhry <rohitchaudhry362@gmail.com>
2021-08-26 10:29:44 -0400

    JS Array Find method example added

---
+++ JavaScript Find Method.js
@@ -0,0 +1,7 @@
+// JavScript Find method returns the first element that satisfies the condition
+
+const numbers = [1,2,3,4,5,6,7,8,9]
+
+const output = numbers.find(number => number > 7);
+
+console.log(output);
